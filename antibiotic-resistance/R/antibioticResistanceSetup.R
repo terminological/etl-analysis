93e838c3e43b506eb0acfe6543175d2bd2b8ea86
Rob Challen <rob@terminological.co.uk>
2020-02-11 10:20:53 +0000

    Abx res

---
+++ antibiotic-resistance/R/antibioticResistanceSetup.R
@@ -0,0 +1,22 @@
+
+omop = Omop$new()
+labTestValues = omop$measurement %>% 
+    filter(!is.na(value_as_concept_id)) %>% 
+    group_by(measurement_concept_id,value_as_concept_id) %>% 
+    summarise(count=n()) %>% 
+    omop$getConceptNames() %>% 
+    arrange(desc(count))
+  
+testConcepts = Searcher$fromDataframe(labTestValues,measurement_concept_id)
+valueConcepts = Searcher$fromDataframe(labTestValues,value_as_concept_id)
+  
+  #labTestValues %>% collect()
+  
+bacteriaIdentified = testConcepts$clone()$applyFilter(
+    (concept_name %like% "%Bacteria identifi%")
+  )
+  
+resistanceOutcome = valueConcepts$clone()$applyFilter(
+    ((concept_name %like% "%sensiti%") |
+       (concept_name %like% "%resis%")) & !(concept_name %like% "%ethicill%")
+  )
